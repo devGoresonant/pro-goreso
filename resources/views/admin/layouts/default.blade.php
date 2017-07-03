610922eca9d377687333cba83a29ddbf1d0f3bf6
Hamender <hamender@goresonant.com>
2017-07-03 16:04:27 +0530

    redirect issue solve

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -10,36 +10,36 @@
 
         <title>Creative - Bootstrap Admin Template</title>
 
-        <!-- Bootstrap CSS -->    
-        <link href="{{ URL::asset('assets/admin/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ URL::asset('assets/admin/css/urban-css.css') }}" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link href="{{ url('public/assets/admin/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ url('public/assets/admin/css/urban-css.css') }}" rel="stylesheet">
         <!-- bootstrap theme -->
-        <link href="{{ URL::asset('assets/admin/css/bootstrap-theme.css') }}" rel="stylesheet">
+        <link href="{{ url('public/assets/admin/css/bootstrap-theme.css') }}" rel="stylesheet">
         <!--external css-->
         <!-- font icon -->
-        <link href="{{ URL::asset('assets/admin/css/elegant-icons-style.css') }}" rel="stylesheet" />
-        <link href="{{ URL::asset('assets/admin/css/font-awesome.min.css') }}" rel="stylesheet" />    
+        <link href="{{ url('public/assets/admin/css/elegant-icons-style.css') }}" rel="stylesheet" />
+        <link href="{{ url('public/assets/admin/css/font-awesome.min.css') }}" rel="stylesheet" />    
         <!-- full calendar css-->
-        <link href="{{ URL::asset('assets/admin/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css') }}" rel="stylesheet" />
-        <link href="{{ URL::asset('assets/admin/assets/fullcalendar/fullcalendar/fullcalendar.css') }}" rel="stylesheet" />
+        <link href="{{ url('public/assets/admin/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css') }}" rel="stylesheet" />
+        <link href="{{ url('public/assets/admin/assets/fullcalendar/fullcalendar/fullcalendar.css') }}" rel="stylesheet" />
         <!-- easy pie chart-->
-        <link href="{{ URL::asset('assets/admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css') }}" rel="stylesheet" type="text/css" media="screen"/>
+        <link href="{{ url('public/assets/admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css') }}" rel="stylesheet" type="text/css" media="screen"/>
         <!-- owl carousel -->
-        <link rel="stylesheet" href="{{ URL::asset('assets/admin/css/owl.carousel.css') }}" type="text/css">
-        <link href="{{ URL::asset('assets/admin/css/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ url('public/assets/admin/css/owl.carousel.css') }}" type="text/css">
+        <link href="{{ url('public/assets/admin/css/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet">
         <!-- Custom styles -->
-        <link rel="stylesheet" href="{{ URL::asset('assets/admin/css/fullcalendar.css') }}">
-        <link href="{{ URL::asset('assets/admin/css/widgets.css') }}" rel="stylesheet">
-        <link href="{{ URL::asset('assets/admin/css/style.css') }}" rel="stylesheet">
-        <link href="{{ URL::asset('assets/admin/css/style-responsive.css') }}" rel="stylesheet" />
-        <link href="{{ URL::asset('assets/admin/css/xcharts.min.css') }}" rel=" stylesheet">	
-        <link href="{{ URL::asset('assets/admin/css/jquery-ui-1.10.4.min.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ url('public/assets/admin/css/fullcalendar.css') }}">
+        <link href="{{ url('public/assets/admin/css/widgets.css') }}" rel="stylesheet">
+        <link href="{{ url('public/assets/admin/css/style.css') }}" rel="stylesheet">
+        <link href="{{ url('public/assets/admin/css/style-responsive.css') }}" rel="stylesheet" />
+        <link href="{{ url('public/assets/admin/css/xcharts.min.css') }}" rel=" stylesheet">	
+        <link href="{{ url('public/assets/admin/css/jquery-ui-1.10.4.min.css') }}" rel="stylesheet">
 
         <!-- javascripts -->
-        <script src="{{ URL::asset('assets/admin/js/jquery.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery-ui-1.10.4.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery-1.8.3.min.js') }}"></script>
-        <script type="text/javascript" src="{{ URL::asset('assets/admin/js/jquery-ui-1.9.2.custom.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery-ui-1.10.4.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery-1.8.3.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('public/assets/admin/js/jquery-ui-1.9.2.custom.min.js') }}"></script>
 
     </head>
     <?php $isLogin = Auth::check(); ?>
@@ -55,40 +55,40 @@
         </section>
 
         <!-- bootstrap -->
-        <script src="{{ URL::asset('assets/admin/js/bootstrap.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/bootstrap.min.js') }}"></script>
         <!-- nice scroll -->
-        <script src="{{ URL::asset('assets/admin/js/jquery.scrollTo.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.nicescroll.js') }}" type="text/javascript"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.scrollTo.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.nicescroll.js') }}" type="text/javascript"></script>
         <!-- charts scripts -->
-        <script src="{{ URL::asset('assets/admin/assets/jquery-knob/js/jquery.knob.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.sparkline.js') }}" type="text/javascript"></script>
-        <script src="{{ URL::asset('assets/admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/owl.carousel.js') }}" ></script>
+        <script src="{{ url('public/assets/admin/assets/jquery-knob/js/jquery.knob.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.sparkline.js') }}" type="text/javascript"></script>
+        <script src="{{ url('public/assets/admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/owl.carousel.js') }}" ></script>
         <!-- jQuery full calendar -->
-        <<script src="{{ URL::asset('assets/admin/js/fullcalendar.min.js') }}"></script> <!-- Full Google Calendar - Calendar -->
-        <script src="{{ URL::asset('assets/admin/assets/fullcalendar/fullcalendar/fullcalendar.js') }}"></script>
+        <<script src="{{ url('public/assets/admin/js/fullcalendar.min.js') }}"></script> <!-- Full Google Calendar - Calendar -->
+        <script src="{{ url('public/assets/admin/assets/fullcalendar/fullcalendar/fullcalendar.js') }}"></script>
         <!--script for this page only-->
-        <script src="{{ URL::asset('assets/admin/js/calendar-custom.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.rateit.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/calendar-custom.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.rateit.min.js') }}"></script>
         <!-- custom select -->
-        <script src="{{ URL::asset('assets/admin/js/jquery.customSelect.min.js') }}" ></script>
-        <script src="{{ URL::asset('assets/admin/assets/chart-master/Chart.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.customSelect.min.js') }}" ></script>
+        <script src="{{ url('public/assets/admin/assets/chart-master/Chart.js') }}"></script>
 
         <!--custome script for all page-->
-        <script src="{{ URL::asset('assets/admin/js/scripts.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/scripts.js') }}"></script>
         <!-- custom script for this page-->
-        <script src="{{ URL::asset('assets/admin/js/sparkline-chart.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/easy-pie-chart.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery-jvectormap-world-mill-en.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/xcharts.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.autosize.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.placeholder.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/gdp-data.js') }}"></script>	
-        <script src="{{ URL::asset('assets/admin/js/morris.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/sparklines.js') }}"></script>	
-        <script src="{{ URL::asset('assets/admin/js/charts.js') }}"></script>
-        <script src="{{ URL::asset('assets/admin/js/jquery.slimscroll.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/sparkline-chart.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/easy-pie-chart.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery-jvectormap-1.2.2.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery-jvectormap-world-mill-en.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/xcharts.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.autosize.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.placeholder.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/gdp-data.js') }}"></script>	
+        <script src="{{ url('public/assets/admin/js/morris.min.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/sparklines.js') }}"></script>	
+        <script src="{{ url('public/assets/admin/js/charts.js') }}"></script>
+        <script src="{{ url('public/assets/admin/js/jquery.slimscroll.min.js') }}"></script>
         <script>
 
 //knob
